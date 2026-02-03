2021f722eb461390ec9f278f0afe6d9c12758353
伊藤 駿 <rendya2501@gmail.com>
2026-02-03 23:01:59 +0900

    Refactor VideoGame entity to remove default values for Title, Genre, and ReleaseYear properties, ensuring they are explicitly set during instantiation.

--- src/Domain/Entities/VideoGame.cs
+++ src/Domain/Entities/VideoGame.cs
@@ -3,7 +3,7 @@
 public class VideoGame
 {
     public int Id { get; set; }
-    public required string Title { get; set; } = string.Empty;
-    public required string Genre { get; set; } = string.Empty;
-    public required int ReleaseYear { get; set; } = 1950;
+    public required string Title { get; set; }
+    public required string Genre { get; set; }
+    public required int ReleaseYear { get; set; }
 }
